1d73cd005bf14954ccf3aa582f01b7458e5aa000
K4J4N <kajan.14@cse.mrt.ac.lk>
2017-11-07 15:48:08 +0530

    added jpa and spring data jpa

---
+++ src/main/java/com/k4j4n/repository/ShipwreckRepository.java
@@ -0,0 +1,8 @@
+package com.k4j4n.repository;
+
+import com.k4j4n.model.Shipwreck;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
+
+}
